c7f0e6670bebb6d39d48c3825e9a7fd15df53fce
YautjaX <deepakpsofficial@gmail.com>
2022-04-27 10:32:42 +0530

    added playlist

---
+++ src/video/video.js
@@ -0,0 +1,40 @@
+export const videosList = {
+	"playlistId": "react_videos",
+	"playlist": [
+		{
+			"num": 1,
+			"title": "Netflix The Witcher - Full Original Soundtrack",
+			"id": "DLX62G4lc44",
+			"duration": "26:15",
+			"video": "https://youtu.be/uv54ec8Pg1k"
+		},
+		{
+			"num": 2,
+			"title": "Best of Game of Thrones Soundtrack: Seasons 1-6",
+			"id": "Ke90Tje7VS0",
+			"duration": "34:59",
+			"video": "https://youtu.be/32oQrjSCUTw"
+		},
+		{
+			"num": 3,
+			"title": "Batman v Superman Soundtrack Medley - Complete Film Soundtrack Mix - Hans Zimmer and Junkie XL",
+			"id": "UtIOMUQ7nWM",
+			"duration": "1:11:03",
+			"video": "https://youtu.be/wQ82DtpnNNk"
+		},
+		{
+			"num": 4,
+			"title": "The Dark Knight Trilogy Ultimate Cut",
+			"id": "A9_9gQIkfx4",
+			"duration": "36:33",
+			"video": "https://youtu.be/iXv92OgO4yY"
+		},
+		{
+			"num": 5,
+			"title": "Interstellar Ultimate Cut",
+			"id": "xihoZZU0gao",
+			"duration": "24:38",
+			"video": "https://youtu.be/MW8RKweDoaEo"
+		}
+	]
+}
